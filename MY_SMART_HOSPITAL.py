a37324bede6642efb380cec135e1939e2ab57cb5
minecraftstevenotadit <dellanokoesuma@gmail.com>
2026-06-20 11:57:01 +0700

    Update MY_SMART_HOSPITAL.py

--- MY_SMART_HOSPITAL.py
+++ MY_SMART_HOSPITAL.py
@@ -70,7 +70,7 @@ st.markdown("""
 
 @st.cache_resource
 def load_model():
-  with open('hospital_model/pkl', 'rb') as f:
+  with open('hospital_model.pkl', 'rb') as f:
     return pickle.load(f)
 
 bundle = load_model()
